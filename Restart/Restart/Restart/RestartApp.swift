d84482659ac7a514062ee56dc61b51fe0fbff86c
Elliiie <eli.georgieva.06@gmail.com>
2023-04-20 15:10:52 +0300

    Add Restart project

---
+++ Restart/Restart/Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Elena Georgieva on 19.04.23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restart/Restart/Restart/Screens/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by Elena Georgieva on 19.04.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    
+    var body: some View {
+        ZStack {
+            if isOnboardingViewActive {
+                OnboardingView()
+            } else {
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
