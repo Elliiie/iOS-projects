ea45cb2824ec52b5ed1c29d40c019b38d516ceb4
Elliiie <eli.georgieva.06@gmail.com>
2023-04-20 17:20:14 +0300

    Add image controls

---
+++ Pinch/Pinch/Pinch/Views/ControlImageView.swift
@@ -0,0 +1,25 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Elena Georgieva on 20.04.23.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+struct ControlImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlImageView(icon: "minus.magnifyingglass")
+            .preferredColorScheme(.dark)
+    }
+}
