b9fee222d1921690cdd1e851ff40452180534805
Elliiie <eli.georgieva.06@gmail.com>
2023-04-20 16:19:39 +0300

    Setup Pinch project

---
+++ Pinch/Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Elena Georgieva on 20.04.23.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
