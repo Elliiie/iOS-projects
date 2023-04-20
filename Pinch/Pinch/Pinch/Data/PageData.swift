f269a99b233378a7bde6b75bfb8fa55d55209ae2
Elliiie <eli.georgieva.06@gmail.com>
2023-04-20 17:51:08 +0300

    Add Thumbnails UI

---
+++ Pinch/Pinch/Pinch/Data/PageData.swift
@@ -0,0 +1,13 @@
+//
+//  PageData.swift
+//  Pinch
+//
+//  Created by Elena Georgieva on 20.04.23.
+//
+
+import Foundation
+
+let pagesData: [Page] = [
+    Page(id: 1, imageName: "magazine-front-cover"),
+    Page(id: 2, imageName: "magazine-back-cover")
+]
