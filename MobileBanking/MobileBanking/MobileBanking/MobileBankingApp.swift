3ed272f78350c39c006141991889fbc6ddc87996
Elliiie <eli.georgieva.06@gmail.com>
2024-07-16 14:52:17 +0300

    Initial commit

---
+++ MobileBanking/MobileBanking/MobileBanking/MobileBankingApp.swift
@@ -0,0 +1,32 @@
+//
+//  MobileBankingApp.swift
+//  MobileBanking
+//
+//  Created by Elena Georgieva on 16.07.24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MobileBankingApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
